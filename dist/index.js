33779e0ac60ada64bf3bc8443e6f684a60f2db1f
oscar marina <oscar7600@outlook.com>
2024-11-24 16:24:03 +0100

    fix: rename node to root for clarity

--- dist/index.js
+++ dist/index.js
@@ -21,17 +21,17 @@ const y = (t, e) => {
   for (const s of t)
     e || (e = s), n = s;
   return [e, n];
-}, m = (t) => (t || "").replace(/<!--[\s\S]*?-->/g, ""), p = (t) => m(t).replace(/\s\s+/g, ""), d = (t, e) => {
+}, m = (t) => (t || "").replace(/<!--[\s\S]*?-->/g, ""), p = (t) => m(t).replace(/\s\s+/g, ""), r = (t, e) => {
   ((t == null ? void 0 : t.nodeType) === 1 || (t == null ? void 0 : t.nodeType) === 11) && Array.isArray(e) && (t.nodeType === 1 && e.forEach(
     (n) => (
       /** @type {HTMLElement} */
       t.removeAttribute(n)
     )
-  ), [...t.childNodes].forEach((n) => d(n, e)));
+  ), [...t.childNodes].forEach((n) => r(n, e)));
 }, w = (t, e = []) => {
   const n = t instanceof HTMLElement, s = t instanceof ShadowRoot;
   if (n || s) {
-    e.length > 0 && d(t, e);
+    e.length > 0 && r(t, e);
     const o = s ? t.innerHTML : t.outerHTML;
     return p(o);
   }
@@ -50,16 +50,16 @@ const y = (t, e) => {
     /** @type {ShadowRoot | *} */
     ((s = t.shadowRoot) == null ? void 0 : s.delegatesFocus) ?? !1
   ) : !1;
-}, H = (t = 10) => Math.random().toString(36).substring(2, t), r = (t, e, n) => {
+}, H = (t = 10) => Math.random().toString(36).substring(2, t), d = (t, e, n) => {
   e.bubbles && (!t.shadowRoot || e.composed) && e.stopPropagation();
   const { bubbles: s, cancelable: o, composed: i } = e, a = e instanceof CustomEvent ? e.detail : null, c = { composed: i, bubbles: s, cancelable: o, detail: a, ...n }, f = n ? [e.type, { ...e, ...c }] : [e.type, e], u = Reflect.construct(e.constructor, f), l = t.dispatchEvent(u);
   return l || e.preventDefault(), l;
 }, R = (t, e, n) => {
   if (typeof e == "string") {
     const s = e, o = new CustomEvent(s);
-    return r(t, o, n);
+    return d(t, o, n);
   }
-  return r(t, e, n);
+  return d(t, e, n);
 }, S = (t) => {
   const e = typeof t == "string" ? new URL(t) : t, n = {};
   for (const s in e)
@@ -67,7 +67,7 @@ const y = (t, e) => {
   return n;
 };
 function* b({
-  node: t,
+  root: t,
   whatToShow: e = 0,
   filterAccept: n = () => !0,
   filterReject: s = () => !1
@@ -77,7 +77,7 @@ function* b({
   n(t) && (yield t);
   const o = t instanceof HTMLElement && t.shadowRoot ? t.shadowRoot.children : t instanceof HTMLSlotElement ? t.assignedNodes({ flatten: !0 }) : t.childNodes;
   for (const i of o)
-    yield* b({ node: i, whatToShow: e, filterAccept: n, filterReject: s });
+    yield* b({ root: i, whatToShow: e, filterAccept: n, filterReject: s });
 }
 export {
   E as composedAncestors,
@@ -90,7 +90,7 @@ export {
   L as isFocusable,
   H as randomID,
   R as redispatchEvent,
-  r as redispatchEventFromEvent,
+  d as redispatchEventFromEvent,
   S as urlToPlainObject,
   b as walkComposedTree
 };
--- dist/walkComposedTree.d.ts
+++ dist/walkComposedTree.d.ts
@@ -3,14 +3,14 @@
  * You can pass [NodeFilter](https://developer.mozilla.org/en-US/docs/Web/API/NodeFilter) or 0 to retrieve all nodes.
  *
  * @param {Object} options - The options object.
- * @param {Node} options.node - The root node for traversal. **Required**.
+ * @param {Node} options.root - The root node for traversal. **Required**.
  * @param {number} [options.whatToShow=0] - NodeFilter code for node types to include. Defaults to `0`, which selects all node types.
  * @param {function} [options.filterAccept=(node: Node) => true] - Filters nodes. Child nodes are considered even if the parent does not satisfy the filter. Defaults to a function that always returns `true`.
  * @param {function} [options.filterReject=(node: Node) => false] - Filters nodes to reject. Any node in the subtree based at this node is not included. Defaults to a function that always returns `false`.
  * @returns {IterableIterator<Node>} An iterator yielding nodes meeting the filter criteria.
  */
-export function walkComposedTree({ node, whatToShow, filterAccept, filterReject, }: {
-    node: Node;
+export function walkComposedTree({ root, whatToShow, filterAccept, filterReject, }: {
+    root: Node;
     whatToShow?: number;
     filterAccept?: Function;
     filterReject?: Function;
